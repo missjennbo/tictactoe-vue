a90b345c453ba1a1c1b8229f09e90a8741d92168
Jennifer Lucifero <jennifer.lucifero@codecentric.de>
2021-11-26 13:41:06 +0100

    Deploy S3 bucket without content

--- deployment/index.ts
+++ deployment/index.ts
@@ -5,12 +5,5 @@ import * as awsx from "@pulumi/awsx";
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket", {website: {indexDocument: "index.html"}});
 
-const bucketObject = new aws.s3.BucketObject("index.html", {
-    acl: "public-read",
-    bucket: bucket,
-    contentType: "text/html",
-    source: new pulumi.asset.FileAsset("index.html")
-});
-
 export const bucketName = bucket.id;
 export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
